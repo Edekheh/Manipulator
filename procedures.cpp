2e2f5f4cc5e1dbe5cc9d58ffca98a24efcd0b179
Edekheh <dawidmoscicki@gmail.com>
2020-07-04 18:55:18 +0200

    SmallUpdateProcedures

--- procedures.cpp
+++ procedures.cpp
@@ -1,13 +1,13 @@
 #include "procedures.h"
 
 void procedureG0(char **commands){
-
+//void mov0(int axis, int deg);
 }
 void procedureG1(char **commands){
-
+//void mov1(int axis, int pos);
 }
 void procedureG2(char **commands){
-
+//void mov2(int axis1,int pos1,int axis2,int pos2);
 }
 void procedureG3(char **commands){
 
