68acf251920ae5ebebb541cdf62ee9fc37281368
Karol Szymczyk <52451542+gogettp11@users.noreply.github.com>
2020-07-05 12:57:07 +0200

    Works? 0.1

--- procedures.cpp
+++ procedures.cpp
@@ -1,26 +1,46 @@
 #include "procedures.h"
 
 void procedureG0(char **commands){
-//mov0(int axis, int deg);
+    char *tempData = commands[1];
+    int axis = tempData.charAt(0)-65;
+    int deg = atoi(tempData);
+    mov0(int axis, int deg);
 }
 void procedureG1(char **commands){
-//mov1(int axis, int pos);
+    char *tempData = commands[1];
+    int axis = tempData.charAt(0)-65;
+    int pos = atoi(tempData);
+    mov1(int axis, int pos);
 }
 void procedureG2(char **commands){
-//mov2(int axis1,int pos1,int axis2,int pos2);
+    char *tempData = commands[1];
+    int axis1 = tempData.charAt(0)-65;
+    int pos1 = atoi(tempData);
+    tempData = commands[2];
+    int axis2 = tempData.charAt(0)-65;
+    int pos2 = atoi(tempData);
+    mov2(int axis1,int pos1,int axis2,int pos2);
 }
 void procedureG3(char **commands){
-//mov3(int axis1, int pos1, int axis2, int pos2,int axis3,int pos3);
+    char *tempData = commands[1];
+    int axis1 = tempData.charAt(0)-65;
+    int pos1 = atoi(tempData);
+    mov3(int axis1, int pos1, int axis2, int pos2,int axis3,int pos3);
 }
 void procedureG4(char **commands){
+    
 
 }
 void procedureG5(char **commands){
     
 }
 void procedureG10(char **commands)  {
-//movServo(int deg);
+    char *tempData = commands[1];
+    int deg = atoi(tempData);
+    movServo(int deg);
 }
 void procedureG11(char **commands)  {
-//movServo(int deg);
+    char *tempData = commands[1];
+    int deg = atoi(tempData);
+    movServo(int deg);
 }
